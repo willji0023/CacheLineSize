6bfe5e367074f030d8f8b9a840919115b254fd77
Dante Tanjuatco <75285586+Upleft-DT@users.noreply.github.com>
2022-02-03 20:13:51 -0800

    Take the average of 1000 trials

--- cachelinesize.c
+++ cachelinesize.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,25 +10,44 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sched.h>
+#include <sys/resource.h>
 #include "cacheutils.h"
 
-int main (int ac, char **av) {		
+
+int main (int ac, char **av) {
+	
+	cpu_set_t mask;
+	CPU_ZERO(&mask);
+	CPU_SET(0, &mask);
+	sched_setaffinity(0, sizeof(mask), &mask);
+	setpriority(PRIO_PROCESS, getpid(), 0);
+	nice(19);
+	
+	int times[2048] = {0};
 	long maxStride = 2048;
-	long maxRange = 1048576;
+	long maxRange = 32000000;
 	FILE *file =  fopen("results.csv", "w+");
 	fputs("stride,iteration,latency\n", file);
-	char* array = (char *)malloc(maxRange + getpagesize());
-
-	for (long stride = 1; stride <= maxStride; stride*=2) {
-		for(int i = 0; i < 100; i++){
-			flush(array + i*stride);
+	char* array = (char*)malloc(maxRange);
+	for(int i = 0; i < 1000; i++){
+		for (long stride = 1; stride <= maxStride; stride*=2) {
+			for(int i = 0; i < 2; i++){
+				flush(array + (i*stride));
+			}
+			size_t time = rdtsc();
+			for(int i = 0; i < 2; i++){
+				maccess(array + (i*stride));
+			}
+			size_t delta = rdtsc() - time;
+			for(int i = 0; i < 2; i++){
+				flush(array + (i*stride));
+			}
+			times[stride - 1] += delta;
 		}
-		size_t time = rdtsc();
-		for(int i = 0; i < 100; i++){
-			maccess(array + i*stride);
-		}
-		size_t delta = rdtsc() - time;
-		fprintf(file, "%ld,%lu\n", stride, delta);
-		printf("%ld,%lu\n",  stride, delta);
 	}
+	for(int stride = 1; stride <= maxStride; stride*=2){
+		printf("%d,%d\n", stride, times[stride-1]/1000);
+	}
+	free(array);
 }
