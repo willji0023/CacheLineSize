a72040fbde08f843c840a1d78dabb17e14e6ebf4
William Jin <wjin0702@yahoo.com>
2022-02-10 20:09:28 -0800

    Fixed indenting for cachesize and associativity and fixed associativity compile issues

--- associativity.c
+++ associativity.c
@@ -16,22 +16,22 @@
 
 
 int main (int ac, char **av) {
-    cpu_set_t mask;
+    	cpu_set_t mask;
 	CPU_ZERO(&mask);
 	CPU_SET(0, &mask);
 	sched_setaffinity(0, sizeof(mask), &mask);
 	setpriority(PRIO_PROCESS, getpid(), 0);
 	nice(19);
-    int times[2048] = {0};
+    	int times[2048] = {0};
 	long maxStride = 2048;
 	long maxRange = 32000000;
 	FILE *file =  fopen("results.csv", "w+");
 	fputs("iteration,latency\n", file);
 	char* array = (char*)malloc(maxRange); //[0] [cachesize*1] [cachesize*2][cachesize*3]
 
-    int cachesize = 64000; //in kb
+    	int cachesize = 64000; //in kb
 
-    for (int i = 0 ; i < 16 ; i++){
+	int i = 16;
         for (int j = 0 ; j < i ; j++){
             maccess(array+j*cachesize);
         }
@@ -45,7 +45,7 @@ int main (int ac, char **av) {
 	    }
 	times[i - 1] += delta;
     
-    for(int stride = 1; stride <= maxStride; stride*=2){
+    	for(int stride = 1; stride <= maxStride; stride*=2){
 		printf("%d,%d\n", stride, times[stride-1]);
 	}
 	free(array);
--- cachesize.c
+++ cachesize.c
@@ -21,30 +21,28 @@ int main (int ac, char **av) {
 	sched_setaffinity(0, sizeof(mask), &mask);
 	setpriority(PRIO_PROCESS, getpid(), 0);
 	nice(19);
-    int times[2048] = {0};
+    	int times[2048] = {0};
 	long maxStride = 2048;
 	long maxRange = 32000000;
 	FILE *file =  fopen("results.csv", "w+");
 	fputs("iteration,latency\n", file);
 	char* array = (char*)malloc(maxRange);
 
-    int cachelinesize = 64; //in bytes
-    //for (int i = 1 ; i < 10 ;i++){
-    int i = 10;
-    for (int j = 0 ; j < i ; j++){
-        maccess(array+j*cachelinesize);
-    }
-    size_t time = rdtsc();
-    for (int j = 0 ; j < i ; j++){
-        maccess(array+j*cachelinesize);
-    }
-    size_t delta = rdtsc() - time;
+    	int cachelinesize = 64; //in bytes
+    	int i = 10;
+    	for (int j = 0 ; j < i ; j++){
+        	maccess(array+j*cachelinesize);
+    	}
+    	size_t time = rdtsc();
+    	for (int j = 0 ; j < i ; j++){
+       		maccess(array+j*cachelinesize);
+    	}
+    	size_t delta = rdtsc() - time;
 	for(int j = 0; j < i; j++){
 		flush(array + (j*cachelinesize));
 	}
 	times[i - 1] += delta;
-    //}
-    for(int stride = 1; stride <= maxStride; stride*=2){
+    	for(int stride = 1; stride <= maxStride; stride*=2){
 		printf("%d,%d\n", stride, times[stride-1]);
 	}
 	free(array);
